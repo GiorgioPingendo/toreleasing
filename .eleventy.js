517da17693fb651957e021b03cd2dfa1eedb7d10
Giorgio Seregni <giorgio.seregni@pingendo.com>
2024-01-19 12:25:59 +0100

    to

--- .eleventy.js
+++ .eleventy.js
@@ -2,4 +2,11 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("media");
     eleventyConfig.addPassthroughCopy("assets");
     eleventyConfig.addPassthroughCopy("admin");
+
+
+    eleventyConfig.addCollection("post", function(collectionApi) {
+        // Restituisci tutti i file nella cartella posts
+        return collectionApi.getFilteredByGlob("./posts/*.md");
+    });
+
 };
