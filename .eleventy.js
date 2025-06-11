a0a40b4da2fe714d5877e92d4e305d684468e15a
Giorgio Seregni <giorgio.seregni@finwave.it>
2025-06-11 22:12:35 +0200

    more

--- .eleventy.js
+++ .eleventy.js
@@ -6,9 +6,21 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy(".env");
     eleventyConfig.addPassthroughCopy("functions");
 
+    // Collezione posts esistente
     eleventyConfig.addCollection("post", function(collectionApi) {
         // Restituisci tutti i file nella cartella posts
         return collectionApi.getFilteredByGlob("./posts/*.md");
+    });
+
+    // Nuova collezione FAQ
+    eleventyConfig.addCollection("faq", function(collectionApi) {
+        // Restituisci tutti i file nella cartella faq ordinati per campo 'order'
+        return collectionApi.getFilteredByGlob("./faq/*.md").sort(function(a, b) {
+            // Ordina per il campo 'order', se non presente usa 999 come default
+            let orderA = a.data.order || 999;
+            let orderB = b.data.order || 999;
+            return orderA - orderB;
+        });
     });
 
     eleventyConfig.on('afterBuild', () => {
